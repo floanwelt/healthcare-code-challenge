7e440fc6ac467409eaf739065744e8911682b4b6
floanwelt1 <floanwelt@gmail.com>
2019-12-22 22:56:53 +0100

    Created Xcode Project

---
+++ healthcare-code-challenge/healthcare-code-challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  healthcare-code-challenge
+//
+//  Created by Florian Abel on 22.12.19.
+//  Copyright © 2019 Florian Abel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
