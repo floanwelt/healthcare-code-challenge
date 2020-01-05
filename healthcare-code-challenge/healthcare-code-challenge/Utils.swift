2fd2a7227efcae6138561221e62629be237881c0
floanwelt1 <floanwelt@gmail.com>
2020-01-05 23:55:03 +0100

    Utils for the use of Imebra library

--- healthcare-code-challenge/healthcare-code-challenge/Utils.swift
+++ healthcare-code-challenge/healthcare-code-challenge/Utils.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+class Utils {
+    
+    static func generateImage(chain: ImebraTransformsChain, image: ImebraImage) -> UIImage? {
+        do {
+            let draw = ImebraDrawBitmap(transform: chain)
+            return try draw?.getImebraImage(image)
+        } catch {
+            return nil
+        }
+    }
+    
+    static func applyTransformation(colorSpace: String?,
+                                    dataSet: ImebraDataSet,
+                                    image: ImebraImage,
+                                    width: UInt32,
+                                    height: UInt32) -> ImebraTransformsChain? {
+        let chain = ImebraTransformsChain()
+        do {
+            if(ImebraColorTransformsFactory.isMonochrome(colorSpace)) {
+
+                let voiDescription = try ImebraVOILUT.getOptimalVOI(image, inputTopLeftX: 0, inputTopLeftY: 0, inputWidth: width, inputHeight: height)
+                let voilutTransform = ImebraVOILUT(voiDescription: voiDescription)
+                chain?.add(voilutTransform)
+            }
+        } catch {
+            print("Error occured")
+        }
+        return chain
+    }
+    
+}
